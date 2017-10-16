2c720884d703d7125f77ae7c1c052e4288451fcf
SSabu <sandeepsabu09@gmail.com>
2017-10-16 16:03:49 -0700

    Add New Path function

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ let shapefile = '../shapefiles/trial2';
 let data = {};
 
 function getGeoJson(geojson) {
-  console.log(geojson);
   data.bbox = geojson.features[0].geometry.bbox,
   data.coordinates = geojson.features[0].geometry.coordinates,
-  data.type = geojson.features[0].geometry.type
+  data.type = geojson.features[0].geometry.type,
+  data.orderedCoordinates = geojson.features[0].geometry.coordinates;
+
+// remove duplicate coordinates and elevation point for array of coordinates
+  data.orderedCoordinates = removeDuplicates(data.orderedCoordinates);
+  data.orderedCoordinates.forEach((coordinate) => coordinate.splice(2));
+
   return data;
 }
 
@@ -106,21 +111,117 @@ function orderIntersections(intersections) {
 
   orderedIntersections.forEach((el) => el.sort((a,b)=>a[1]-b[1]));
 
+// structure intersections similar to coordinate array with [long, lat]
+
+  orderedIntersections.forEach((el) => el.forEach((el) => el.reverse()));
+
   data.orderedIntersections = orderedIntersections;
+
+  return data;
+}
+
+// find coordinate in a set between two points
+
+function nextPoint(point1, point2, arrayOfPoints) {
+
+  for (let i=0; i<data.orderedCoordinates.length; i++) {
+
+    if (arrayOfPoints[i][0]>point2[0] && arrayOfPoints[i][0]<point1[0] && arrayOfPoints[i][1]<point2[1] && arrayOfPoints[i][1]>point1[1]
+    ) {
+      return arrayOfPoints[i];
+    }
+  }
+}
+
+//transform array based on which direction path travels first
+
+function intersectionsPathLeft(intersections) {
+
+  let flattenedArray = [];
+
+  for (let i=0; i<intersections.length; i++) {
+    if (i%2!==0) {
+      intersections[i].sort((a,b)=>b[0]-a[0]);
+    }
+    if (i%2===0) {
+      intersections[i].sort((a,b) => a[0]-b[0])
+    }
+  }
+  intersections.forEach((el) => el.forEach((el) => flattenedArray.push(el)));
+
+  return flattenedArray;
+}
+
+
+function intersectionsPathRight(intersections) {
+
+  let flattenedArray = [];
+
+  for (let i=0; i<intersections.length; i++) {
+    if (i%2!==0) {
+
+      intersections[i].sort((a,b)=>a[0]-b[0]);
+    }
+    if (i%2==0) {
+      intersections[i].sort((a,b) => b[0]-a[0]);
+    }
+  }
+  intersections.forEach((el) => el.forEach((el) => flattenedArray.push(el)));
+
+  return flattenedArray;
+}
+
+//create drone path
+
+function createPath(bbox, orderedIntersections, orderedCoordinates) {
+
+  let newPath = [];
+  let newIntersections = [];
+
+  data.orderedCoordinates.forEach(function(el) {
+    if (el[1]===data.bbox[1]) {
+      newPath.push(el);
+    }
+  });
+
+  if (nextPoint(newPath[newPath.length-1], data.orderedIntersections[0][0], data.orderedCoordinates) ) {
+    newPath.push(nextPoint(newPath[newPath.length-1], data.orderedIntersections[0][0], data.orderedCoordinates));
+    newIntersections = intersectionsPathLeft(data.orderedIntersections);
+    newPath.push(newIntersections[0], newIntersections[1]);
+  }
+
+  else if (nextPoint(newPath[newPath.length-1], data.orderedIntersections[0][1], data.orderedCoordinates)) {
+    newPath.push(nextPoint(newPath[newPath.length-1], data.orderedIntersections[0][1], data.orderedCoordinates));
+    newIntersections = intersectPathRight(data.orderedIntersections);
+    newPath.push(newIntersections[0], newIntersections[1])
+  }
+
+  else {
+    newIntersections = intersectionsPathLeft(data.orderedIntersections);
+    newPath.push(newIntersections[0], newIntersections[1]);
+  }
+
+  for (let i=2; i<newIntersections.length; i+=2) {
+  if(nextPoint(newPath[newPath.length-1], newIntersections[i], data.orderedCoordinates) ) {
+    newPath.push(nextPoint(newPath[newPath.length-1], newIntersections[i], data.orderedCoordinates));
+  }
+
+  newPath.push(newIntersections[i], newIntersections[i+1]);
+
+}
+
+  data.newPath = newPath;
 
   return data;
 }
 
 
 
-
 shp(shapefile).then(getGeoJson)
   .then(findIntersections)
   .then(orderIntersections)
+  .then(createPath)
   .then(console.log)
   .catch(err=>console.log(err));
 
   console.log('THIS IS DATA',data);
-
-//
-// console.log('TRIAL', boundary, parallelLines);
